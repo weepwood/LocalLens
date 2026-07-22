9dc85f945fb1674dd3341a51fbd48d15145de5bb
weepwood <weepwood@outlook.com>
2026-07-23 01:44:47 +0800

    修复原生 Android Compose 权重导入

--- apps/local_lens_android/app/src/main/java/com/weepwood/locallens/MainActivity.kt
+++ apps/local_lens_android/app/src/main/java/com/weepwood/locallens/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
